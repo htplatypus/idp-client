13ba8cee92e47d8c0a83794ac2be2ade69e4f7c7
htplatypus <aleksic@protonmail.ch>
2024-11-23 21:57:57 +0100

    init populate

---
+++ src/main/java/org/project/idpclient/IdpClientApplication.java
@@ -0,0 +1,13 @@
+package org.project.idpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdpClientApplication.class, args);
+    }
+
+}
